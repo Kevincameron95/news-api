5d0242089233dbdcaf9b63ac41a84b6237394c4d
Kevincameron95 <34334722+Kevincameron95@users.noreply.github.com>
2022-11-21 21:38:06 -0800

    more css changes

--- js/top-by-country.js
+++ js/top-by-country.js
@@ -1,44 +1,44 @@
 // Canada
-function canadaHeadline(response){
-    axios.get('//newsapi.org/v2/top-headlines?country=ca&apiKey=447b8c1c1861481a95578726345b1cf9', {
-        params: {
-            ID:12345
-        }
-    })
-  .then(function (response) {
-    const canadaData = response.data;
-    const canadaStoriesArray = Array.from(canadaData.articles);
-    canadaStoriesArray.length = 3;
-    // console.log(canadaStoriesArray);
-    // document.querySelector('#canada-target').style.backgroundImage = `url('${canadaStoriesArray[0].urlToImage}')`;
-    document.getElementById('canada-target').style.backgroundImage= `url('${canadaStoriesArray[0].urlToImage}`
+// function canadaHeadline(response){
+//     axios.get('//newsapi.org/v2/top-headlines?country=ca&apiKey=447b8c1c1861481a95578726345b1cf9', {
+//         params: {
+//             ID:12345
+//         }
+//     })
+//   .then(function (response) {
+//     const canadaData = response.data;
+//     const canadaStoriesArray = Array.from(canadaData.articles);
+//     canadaStoriesArray.length = 3;
+//     // console.log(canadaStoriesArray);
+//     // document.querySelector('#canada-target').style.backgroundImage = `url('${canadaStoriesArray[0].urlToImage}')`;
+//     document.getElementById('canada-target').style.backgroundImage= `url('${canadaStoriesArray[0].urlToImage}`
     
 
-    for(let i = 2; i < 3; i++){
-        const canadaMainStoryTarget = document.createElement('div');
-        canadaMainStoryTarget.classList.add('canada-target-div');
+//     for(let i = 2; i < 3; i++){
+//         const canadaMainStoryTarget = document.createElement('div');
+//         canadaMainStoryTarget.classList.add('canada-target-div');
 
-        canadaMainStoryTarget.innerHTML = `
-        <div class="canada-target-headline" >
-        <h2 class="canada-target-headline-h2 target-article-headline">${canadaStoriesArray[0].title}</h2>
+//         canadaMainStoryTarget.innerHTML = `
+//         <div class="canada-target-headline" >
+//         <h2 class="canada-target-headline-h2 target-article-headline">${canadaStoriesArray[0].title}</h2>
         
         
-        `
-        document.getElementById('canada-target').appendChild(canadaMainStoryTarget);
-        // console.log(canadaMainStoryTarget);
+//         `
+//         document.getElementById('canada-target').appendChild(canadaMainStoryTarget);
+//         // console.log(canadaMainStoryTarget);
         
-    }
-    // console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-};
+//     }
+//     // console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+// };
 
-canadaHeadline();
+// canadaHeadline();
 
 function usaHeadline(response){
     axios.get('//newsapi.org/v2/top-headlines?country=us&apiKey=447b8c1c1861481a95578726345b1cf9', {
