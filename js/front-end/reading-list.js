51c7e7adb1b442dbf99bcaef71fab603cbc0b1e6
Kevincameron95 <34334722+Kevincameron95@users.noreply.github.com>
2022-12-13 12:55:51 -0800

    Auto formatting reading list. trying plugin

--- js/front-end/reading-list.js
+++ js/front-end/reading-list.js
@@ -1,15 +1,23 @@
-import {articleOne} from '../backend/requests'
+
 
 const targetID = document.getElementById('reading-list-target');
-
-
-const readingListDiv = document.createElement('div');
-readingListDiv.classList.add('reading-list-div');
-readingListDiv.innerHTML=`
-<div class= "reading-list-article-1">
-<p class="article text">hello ${articleOne}</P>
-</div>
-
-`
-targetID.appendChild(readingListDiv);
-console.log(readingListDiv)
+fetch("../data.json")
+.then(res => res.json())
+.then(function firstArticleResponse(data){
+   
+    const article1data = data;
+    console.log(article1data);
+    const Str = require('@supercharge/strings')
+    const title = Str(' Supercharge is sweet!').trim().title().get()
+    console.log(substring);
+    const readingListDiv = document.createElement('div');
+    readingListDiv.classList.add('reading-list-div');
+    readingListDiv.innerHTML=`
+    <div class= "reading-list-article-1">
+    <p class="reading-list-1">hello ${substring}</P>
+    </div>
+    
+    `
+    targetID.appendChild(readingListDiv);
+    // console.log(readingListDiv)
+})
